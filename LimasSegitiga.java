8363995459a73f517bd2c60c9c27d900c715ed18
Mohammad Alka Ramadhan <99939707+alfaarmdhn@users.noreply.github.com>
2022-02-21 08:24:06 +0700

    Create LimasSegitiga.java

---
+++ LimasSegitiga.java
@@ -0,0 +1,38 @@
+
+package limassegitiga;
+
+public class LimasSegitiga {
+    private double alas;
+    private double tinggiAlas;
+    private double tinggiLimas;
+
+    public double getAlas() {
+        return alas;
+    }
+
+    public void setAlas(double alas) {
+        this.alas = alas;
+    }
+
+    public double getTinggiAlas() {
+        return tinggiAlas;
+    }
+
+    public void setTinggiAlas(double tinggiAlas) {
+        this.tinggiAlas = tinggiAlas;
+    }
+
+    public double getTinggiLimas() {
+        return tinggiLimas;
+    }
+
+    public void setTinggiLimas(double tinggiLimas) {
+        this.tinggiLimas = tinggiLimas;
+    }
+    
+  
+        
+    }
+    
+    
+
